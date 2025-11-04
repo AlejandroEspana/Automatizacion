615249ef192cd282d06f6a7c487e5919e86a6c3b
Smkg911 <alejoespana123@gmail.com>
2025-11-03 20:33:36 -0500

    Integracion de validacion de usuario, ademas se integra validacion con errores dentro de PostMan

--- src/main/java/com/announcements/AutomateAnnouncements/entities/UserProfile.java
+++ src/main/java/com/announcements/AutomateAnnouncements/entities/UserProfile.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Column;
 import java.time.LocalDateTime;
 import lombok.Data;
 
@@ -13,7 +14,10 @@ public class UserProfile {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    
+    @Column(unique = true)
     private String email;
+    
     private String displayName;
     private LocalDateTime createdAt;
 }
